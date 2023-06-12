f418a844c13b18eb7613d28afda9072c4a2f6e62
kornukovov <kornukovov@yandex.ru>
2023-06-12 23:08:13 +0300

    add_HW001

---
+++ HW001/Program.cs
@@ -0,0 +1,45 @@
+﻿// System.Console.Write("введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b) 
+// {
+//    Console.WriteLine($"{a} > {b}");
+// }
+// else
+// {
+//    Console.WriteLine($"{b} > {a}");
+// }
+
+// System.Console.Write("введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine($"максимальное число: {max}");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+// {
+//    Console.WriteLine("Да");
+// } 
+// else 
+// {
+//    Console.WriteLine("Нет");
+// }
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// int curent = 1;
+// while (curent <= N)
+// {
+//    if (curent % 2 == 0)
+//    {
+//       System.Console.Write(curent + ", ");
+//    }
+//    curent++;
+// }
